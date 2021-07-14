ef6d0544e8c0a12e7f2ba308972df9ad7ddd2c18
Alex <anministry.mgmt@gmail.com>
2021-07-14 15:40:22 +0300

    Little change for testing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
-  return <div className='App'>Hello World!</div>;
+  
+  return (
+    <div className='App'>
+      Learning React!
+    </div>)
 }
 
 export default App;
